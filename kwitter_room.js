6594d445fa1c18a2f162994787d0483f3443490b
Swarnim Mayukh <88070589+SwarnimMayukh@users.noreply.github.com>
2022-02-11 10:31:39 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -45,7 +45,8 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
       function support() {
             window.location = "support.html";
       }
-      function join() {
-            window.location = "callstart.html";
-      }
 getData();
+
+function join() {
+      window.location = "callstart.html";
+}
